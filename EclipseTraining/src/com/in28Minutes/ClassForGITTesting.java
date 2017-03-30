ea19f5c93346f50b8d3073ffd362adc7e2586071
Fabio Rossi <acciu75@gmail.com>
2017-03-30 15:09:52 +0200

    first commit for local repo Eclipse Training

---
+++ EclipseTraining/src/com/in28Minutes/ClassForGITTesting.java
@@ -0,0 +1,5 @@
+package com.in28Minutes;
+
+public class ClassForGITTesting {
+
+}
---
+++ EclipseTraining/src/com/in28Minutes/EclipseTrainingClass.java
@@ -0,0 +1,16 @@
+package com.in28Minutes;
+
+public class EclipseTrainingClass {
+	int x;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Integer[] numbers = new Integer[] { 1, 2, 3, 4, 5, 6, 7, 8 };
+		int sum = 0;
+		for (int number : numbers) {
+			sum += number;
+		}
+		System.out.println(sum);
+	}
+
+}
